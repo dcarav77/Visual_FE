b766993fd9fe1a38644ae9d701fafac931827e71
Dustin Caravaglia <dustin_caravaglia@Dustins-Air.lan>
2023-07-24 14:45:28 -0400

    making changes

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route("/")
+def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/calculate", methods=['POST'])
+def calculate():
+    operation = request.form['operation']
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    result = calculate_result(operation, num1, num2)
+    return render_template('index.html', result=result)
+
+if __name__ == "__main__":
+    app.run(port=5000)
+    print("server is running")
