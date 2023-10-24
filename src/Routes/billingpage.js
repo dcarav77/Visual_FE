c472c71a43af4da23eeb9c1a0f1bac7bb5727731
Dustin Caravaglia <dustin_caravaglia@Dustins-Air.lan>
2023-10-24 18:00:06 -0400

    adding routes

---
+++ src/Routes/billingpage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function BillingPage() {
+  return (
+    <iframe
+      src="http://localhost:4000/static/Billing/billing.html"
+      title="Billing Page"
+      width="100%"
+      height="600"
+      frameBorder="0"
+    />
+  );
+}
+
+export default BillingPage;
