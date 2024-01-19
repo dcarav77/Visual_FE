9872c0a362e96ccd8b8a845df1a72fd18e46ac81
Dustin Caravaglia <dustin_caravaglia@Dustins-MacBook-Air.local>
2024-01-19 09:00:28 -0500

    email for footer

---
+++ src/Home_Base/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <p>
+        Contact Email: admin@strongallalong.coach
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
