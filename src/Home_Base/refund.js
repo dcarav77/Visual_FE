27ecaed075d1b1372e416c6c9741b275ef233c33
Dustin Caravaglia <dustin_caravaglia@Dustins-Air.lan>
2024-01-19 12:51:54 -0500

    refund script

---
+++ src/Home_Base/refund.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function RefundPolicy() {
+  return (
+    <div className="refund">
+      <p>
+    The core mission of Strong all Along is to assist men in achieving weight loss and breaking free from the obstacles that hinder their self-respect and personal growth. Our approach involves delivering the most accurate information, top-quality products, and services currently available.
+
+    Regarding our Satisfaction Guarantee - it's important to clarify what it isn't. This guarantee isn't a means to evade personal responsibility for program participation. While this might sound stern, we genuinely believe that a financial investment in oneself often leads to a greater commitment to utilizing the available tools. Our carefully crafted tools empower you to regain control over your lifestyle, mindset, and, ultimately, your self-confidence. Our aim is to educate YOU, enabling the creation of sustainable results. However, it all begins with your active participation.
+
+    As a bonus, membership in our All Access online community comes bundled with all purchases. Nevertheless, it's crucial to note that if you are removed from the group due to non-compliance with the community rules, a refund will not be issued for any purchased products.
+
+    At Strong all Along, we hold a strong belief in the quality of our services, and we are dedicated to ensuring your complete satisfaction with your membership. This commitment is the driving force behind our 100% Satisfaction Guarantee.
+      </p>
+    </div>
+  );
+}
+
+export default RefundPolicy;
