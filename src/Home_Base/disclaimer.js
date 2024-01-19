aa16f847a857657da902d2bf6350380373cb5b5b
Dustin Caravaglia <dustin_caravaglia@Dustins-MacBook-Air.local>
2024-01-19 09:01:23 -0500

    disclaimer for footer

---
+++ src/Home_Base/disclaimer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Disclaimer() {
+  return (
+    <div className="disclaimer">
+      <p>
+        The information provided on this website, including but not limited to text, graphics, video, images, and other materials, is for educational purposes only. It should not be considered a substitute for professional medical advice, diagnosis, or treatment. Always consult with a qualified healthcare provider for any questions related to your health condition, medical concerns, or new healthcare routines. Do not disregard professional medical advice or delay seeking it due to information found on this website.
+      </p>
+      <p>
+        Results May Vary: Testimonials about weight loss are not guarantees of individual results. Weight loss outcomes, including the amount and duration, will differ among individuals. Factors such as genetics, environment, food consumption, metabolism rates, and exercise levels vary from person to person, resulting in variations in weight loss results. No individual result should be considered typical. By using Watson Fit's website, products, or services, you acknowledge that Watson Fit and its owners, distributors, participants, affiliates, employees, agents, and independent contractors are not responsible for any liability or loss associated with the content on this website. You also agree that Watson Fit is not responsible for any products or services recommended on the site and shall not be liable for incidental or consequential damages arising from the use or inability to use the materials/information published or products sold on this site.
+      </p>
+      <p>
+        These statements have not been evaluated by the Food and Drug Administration. The products and services offered are not intended to diagnose, treat, cure, or prevent any disease.
+      </p>
+      <p>
+        The use of information from this site is at your own risk.
+      </p>
+      <p>
+        Advances in medical research may affect the health, fitness, and nutritional advice provided here. No guarantee can be made that the advice on this site will always reflect the latest findings or developments in the field.
+      </p>
+      <p>
+        If you are in the United States and believe you are experiencing a medical or health emergency, please contact a healthcare professional or dial 911 immediately.
+      </p>
+      <p>
+        Some health or medical content on this site may be explicit in nature. If you find such materials offensive, please refrain from using this site.
+      </p>
+    </div>
+  );
+}
+
+export default Disclaimer;
